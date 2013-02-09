6e3fb85ddf75c6977473af4c046d4ae3442029a4
Dirk Breuer <dirk.breuer@gmail.com>
2013-02-09 20:56:38 +0100

    added compass

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem "bootstrap-sass", "~> 2.2.1.1"
+gem "compass"
 gem "sinatra"
 gem "shotgun"
 gem "puma"
---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "sass"
+images_dir = "public/img"
+javascripts_dir = "public/js"
+fonts_dir = "public/fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
